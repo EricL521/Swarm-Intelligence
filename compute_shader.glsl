a5850e4a6eab9132c7a8bb73f7d7fe826a777e59
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-11-06 15:10:15 -0500

    Shader get_index now works and runs for each point

--- compute_shader.glsl
+++ compute_shader.glsl
@@ -6,15 +6,29 @@
 // Invocations in the (x, y, z) dimension
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
-// A binding to the buffer we create in our script
-layout(set = 0, binding = 0, std430) restrict buffer MyDataBuffer {
+// A binding to the data buffer we create in our script
+layout(set = 0, binding = 0, std430) restrict buffer WorldDataBuffer {
     int64_t data[];
 }
-my_data_buffer;
+world_data_buffer;
+
+// Array of: [size_x, size_y, size_z, NUM_DATA_ENTRIES]
+layout(set = 1, binding = 1, std430) restrict buffer WorldSizeBuffer {
+    int data[];
+}
+world_size_buffer;
+
+
+int getIndex(ivec3 pos) {
+    return (pos.x * world_size_buffer.data[1] * world_size_buffer.data[2] * world_size_buffer.data[3])
+        + (pos.y * world_size_buffer.data[2] * world_size_buffer.data[3])
+        + (pos.z * world_size_buffer.data[3]);
+}
 
 // The code we want to execute in each invocation
 void main() {
-    // gl_GlobalInvocationID.x uniquely identifies this invocation across all work groups
-    // my_data_buffer.data.length();
-    my_data_buffer.data[gl_GlobalInvocationID.x] += my_data_buffer.data[gl_GlobalInvocationID.x - 1] - my_data_buffer.data[gl_GlobalInvocationID.x + 1];
+    // gl_GlobalInvocationID uniquely identifies this invocation across all work groups
+    int index = getIndex(ivec3(gl_GlobalInvocationID.xyz));
+    world_data_buffer.data[index] = 1;
+
 }
